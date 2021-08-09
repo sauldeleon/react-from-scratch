a972e8885a43a0a10992ac4446aeb029deb4ac09
Borja Gonzalez <borja@bgon.es>
2021-08-09 20:16:05 +0200

    webpack configuration split by environment

---
+++ config/build/webpack.common.js
@@ -0,0 +1,36 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+export const ROOT_PATH = path.join(__dirname, '../..')
+
+export default {
+  entry: `${ROOT_PATH}/src/index.tsx`,
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.svg$/,
+        loader: '@svgr/webpack',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${ROOT_PATH}/public/index.html`,
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: `${ROOT_PATH}/dist`,
+    clean: true,
+  },
+}
